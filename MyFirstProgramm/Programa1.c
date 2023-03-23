f7ddd4171ae5f9fa33f3e3ee4657b75cea5d656c
Thiago <thiagocoelho.coelho2@gmail.com>
2023-03-23 19:56:10 -0300

    inicio dos estudos

---
+++ MyFirstProgramm/Programa1.c
@@ -0,0 +1,14 @@
+//Varíaveis
+
+#include <stdio.h>
+
+int main()
+{
+    //declarando Variáveis
+    int idade; // inteiro
+
+    //função  para escrever algo na saida padrão(Console)
+    Printf("Qual é a sua idade? ");
+    
+    return 0;
+}
